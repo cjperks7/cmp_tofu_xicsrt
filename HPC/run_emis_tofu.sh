49f8f47dbcc1c69368b624ef48702896849a1702
cjperks7 <cjperk@mit.edu>
2024-12-10 15:39:59 -0500

    shell scripts to run radial emissivity

--- HPC/run_emis_tofu.sh
+++ HPC/run_emis_tofu.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=XICSRT
 #SBATCH -C rocky8
+#SBATCH --array=0
 #SBATCH --time=08:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem-per-cpu=500G
 #SBATCH -p sched_mit_psfc_r8
-#SBATCH --output=output/XRSHRKr_res_v1/slurm.out
-#SBATCH --error=output/XRSHRKr_res_v1/slurm.err
+#SBATCH --output=output/XRSHRKr_emis_v1/slurm_%a.out
+#SBATCH --error=output/XRSHRKr_emis_v1/slurm_%a.err
 #SBATCH --mail-type=BEGIN,END
 #SBATCH --mail-user=cjperks@mit.edu
 
@@ -16,4 +17,4 @@ module purge
 . $HOME/bin/init_R8_py310.sh
 
 # Your job script here
-python resolution_HPC.py XRSHRKr_res_v1
+python rad_emis.py $SLURM_ARRAY_TASK_ID XRSHRKr_emis_v1 tofu
