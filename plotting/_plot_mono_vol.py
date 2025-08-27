70ea3d57d2026b478cac68713986eaab1a9087e5
cjperks7 <cjperk@mit.edu>
2025-08-27 16:48:25 -0400

    Switched units to ph/cm for a meaningful comparison

--- plotting/_plot_mono_vol.py
+++ plotting/_plot_mono_vol.py
@@ -41,29 +41,41 @@ def plt_mono_vol(
     dxi = ddata['XICSRT']
     dtf = ddata['ToFu']
 
-    # Rescale ToFu if different pixel binning
-    scalet_0 = (
-        dtf['npix'][0]/dxi['npix'][0]
-        *dtf['npix'][1]/dxi['npix'][1]
-        )
-    scalet_1 = (
-        dtf['npix'][1]/dxi['npix'][1]
-        )
+    # Normalizing by bin length
+    scalex = {}
+    scalex['h'] = (
+        1
+        /np.mean(np.diff(dxi['cents_cm'][0]))
+        ) # [1/cm], horizontal bin width
+    scalex['v'] = (
+        1
+        /np.mean(np.diff(dxi['cents_cm'][1]))
+        ) # [1/cm], vertical bin width
+    scalex['hv'] = scalex['h']*scalex['v'] # [1/cm^2], bin area
 
-    cmax = np.max((
-        np.max(dxi['signal'].flatten()),
-        np.max(dtf['signal'].flatten())*scalet_0
-        ))
+    scalet = {}
+    scalet['h'] = (
+        1
+        /np.mean(np.diff(dtf['cents_cm'][0]))
+        ) # [1/cm], horizontal bin width
+    scalet['v'] = (
+        1
+        /np.mean(np.diff(dtf['cents_cm'][1]))
+        ) # [1/cm], vertical bin width
+    scalet['hv'] = scalet['h']*scalet['v'] # [1/cm^2], bin area
+
+    label_hv = r'#$ph/cm^2$'
+    label_h = label_v = r'#$ph/cm$'
 
     # Plots photon flux on detector from XICSRT
     fig, ax = plt.subplots(1,2)
     im = ax[1].imshow(
-        dxi['signal'].T, # normalize [# ph] detected by [# ph] emitted
+        dxi['signal'].T*scalex['hv'], # normalize [# ph] detected by [# ph] emitted
         extent = dxi['extent'],
         interpolation='nearest',
         origin='lower',
         vmin=0,
-        vmax=np.max(dxi['signal'].flatten()),
+        vmax=np.max(dxi['signal'].flatten())*scalex['hv'],
         aspect = dxi['aspect']
         )
     cb = plt.colorbar(im, ax=ax, orientation='vertical')
@@ -74,13 +86,13 @@ def plt_mono_vol(
 
 
     im1 = ax[0].imshow(
-        dtf['signal'].T*scalet_0,
+        dtf['signal'].T*scalet['hv'],
         #extent = extent,
         extent = dtf['extent'],
         interpolation='nearest',
         origin='lower',
         vmin=0,
-        vmax=np.max(dtf['signal'].flatten())*scalet_0,
+        vmax=np.max(dtf['signal'].flatten())*scalet['hv'],
         aspect = dtf['aspect']
         )
     #cb = plt.colorbar(im1, ax=ax1, orientation='horizontal')
@@ -89,7 +101,7 @@ def plt_mono_vol(
         ), color = 'red')
     ax[0].set_xlabel('horizontal bin')
     ax[0].set_ylabel('vertical bin')
-    cb.set_label('#ph/bin^2')
+    cb.set_label(label_hv)
 
 
     fig.show()
@@ -103,98 +115,89 @@ def plt_mono_vol(
     # --- Image Slices
     ##########
 
-    indy = int(dxi['npix'][1]/2-1)
-    indx = np.argmax(np.sum(dxi['signal'], axis=1))
-
     indyt = int(dtf['npix'][1]/2-1)
-    indxt = np.argmin(abs(
-        dtf['cents_cm'][0] - dxi['cents_cm'][0][indx]
+    indxt = np.argmax(np.sum(dtf['signal'], axis=1))
+
+    indy = np.argmin(abs(
+        dtf['cents_cm'][1][indyt] - dxi['cents_cm'][1]
+        ))
+    indx = np.argmin(abs(
+        dtf['cents_cm'][0][indxt] - dxi['cents_cm'][0]
         ))
 
-    # Rescale ToFu if different pixel binning
-    scalet_10 = scalet_11 = (
-        dtf['npix'][0]/dxi['npix'][0]
-        *dtf['npix'][1]/dxi['npix'][1]
-        )
-    scalet_01 = (
-        dtf['npix'][1]/dxi['npix'][1]
-        )
-    scalet_00 = (
-        dtf['npix'][0]/dxi['npix'][0]
-        )
 
     fig4, ax4 = plt.subplots(2,2)
 
 
     ax4[0,0].plot(
         dxi['cents_cm'][0],
-        np.sum(dxi['signal'], axis = 1),
+        np.sum(dxi['signal'], axis = 1)*scalex['h'],
         'b*-',
         label = 'XICSRT'
         )
 
     ax4[0,0].plot(
         dtf['cents_cm'][0],
-        np.sum(dtf['signal'], axis = 1) *scalet_00,
+        np.sum(dtf['signal'], axis = 1)*scalet['h'],
         'r*-',
         label = 'ToFu'
         )
 
     ax4[0,0].set_xlabel('horiz. bin [cm]')
-    ax4[0,0].set_ylabel('# photons/bin')
+    ax4[0,0].set_ylabel(label_h)
     ax4[0,0].set_title('int. over all vert. bins')
     ax4[0,0].grid('on')
     ax4[0,0].legend(labelcolor='linecolor')
 
     ax4[1,0].plot(
         dxi['cents_cm'][0],
-        dxi['signal'][:,indy],
+        dxi['signal'][:,indy]*scalex['hv'],
         'b*-'
         )
 
     ax4[1,0].plot(
         dtf['cents_cm'][0],
-        dtf['signal'][:,indyt] *scalet_10,
+        dtf['signal'][:,indyt] *scalet['hv'],
         'r*-'
         )
 
     ax4[1,0].set_xlabel('horiz. bin [cm]')
-    ax4[1,0].set_ylabel('# photons/bin^2')
+    ax4[1,0].set_ylabel(label_hv)
     ax4[1,0].set_title('vert. bin %i/%i'%(indy, dxi['npix'][1]-1))
     ax4[1,0].grid('on')
 
     ax4[0,1].plot(
         dxi['cents_cm'][1],
-        np.sum(dxi['signal'], axis = 0),
+        np.sum(dxi['signal'], axis = 0)*scalex['v'],
         'b*-'
         )
 
     ax4[0,1].plot(
         dtf['cents_cm'][1],
-        np.sum(dtf['signal'], axis = 0) *scalet_01,
+        np.sum(dtf['signal'], axis = 0) *scalet['v'],
         'r*-'
         )
 
     ax4[0,1].set_xlabel('vert. bin [cm]')
-    ax4[0,1].set_ylabel('# photons/bin')
+    ax4[0,1].set_ylabel(label_v)
     ax4[0,1].set_title('int. over all horz. bins')
     ax4[0,1].grid('on')
 
     ax4[1,1].plot(
         dxi['cents_cm'][1],
-        dxi['signal'][indx,:],
+        dxi['signal'][indx,:]*scalex['hv'],
         'b*-'
         )
 
     ax4[1,1].plot(
         dtf['cents_cm'][1],
-        dtf['signal'][indxt,:] *scalet_11,
+        dtf['signal'][indxt,:] *scalet['hv'],
         'r*-'
         )
 
 
     ax4[1,1].set_xlabel('vert. bin [cm]')
-    ax4[1,1].set_ylabel('# photons/bin^2')
+    ax4[1,1].set_ylabel(label_hv)
     ax4[1,1].set_title('horz. bin %i/%i'%(indx, dxi['npix'][0]-1))
     ax4[1,1].grid('on')
 
@@ -203,8 +206,8 @@ def plt_mono_vol(
     ###### ---- Used in paper ---- ######
 
     ymax = np.max((
-        np.max(np.sum(dxi['signal'], axis=0)),
-        np.max(np.sum(dtf['signal'],axis=0))*scalet_1
+        np.max(np.sum(dxi['signal'], axis=0))*scalex['v'],
+        np.max(np.sum(dtf['signal'],axis=0))*scalet['v']
         ))
 
     fig3 = plt.figure(figsize = (16,10))
@@ -219,12 +222,12 @@ def plt_mono_vol(
     ax30 = fig3.add_subplot(gs3[1,0])
 
     im = ax30.imshow(
-        dxi['signal'].T, # normalize [# ph] detected by [# ph] emitted
+        dxi['signal'].T*scalex['hv'], # normalize [# ph] detected by [# ph] emitted
         extent = np.asarray(dxi['extent'])*100,
         interpolation='nearest',
         origin='lower',
         vmin=0,
-        vmax=np.max(dxi['signal'].flatten()),
+        vmax=np.max(dxi['signal'].flatten())*scalex['hv'],
         aspect = 'equal'
         )
 
@@ -235,7 +238,7 @@ def plt_mono_vol(
     ax30.set_ylabel('vert. bin [cm]')
 
     cb = fig3.colorbar(im, ax=ax30, orientation='vertical')
-    cb.set_label(r'signal [#ph/bin$^2$]')
+    cb.set_label(r'signal [%s]'%(label_hv))
 
     ax30.text(0.05, 0.90, '(b)', color = 'w', transform=ax30.transAxes)
 
@@ -245,13 +248,13 @@ def plt_mono_vol(
 
 
     im1 = ax31.imshow(
-        dtf['signal'].T*scalet_0,
+        dtf['signal'].T*scalet['hv'],
         #extent = extent,
         extent = np.asarray(dtf['extent'])*100,
         interpolation='nearest',
         origin='lower',
         vmin=0,
-        vmax=np.max(dtf['signal'].flatten())*scalet_0,
+        vmax=np.max(dtf['signal'].flatten())*scalet['hv'],
         aspect = 'equal'
         )
     ax31.set_title('ToFu, #ph detected = %1.3e'%(
@@ -262,7 +265,7 @@ def plt_mono_vol(
 
     #cb = fig3.colorbar(im, ax=[ax30, ax31], orientation='vertical')
     cb = fig3.colorbar(im, ax=ax31, orientation='vertical')
-    cb.set_label(r'signal [#ph/bin$^2$]')
+    cb.set_label(r'signal [%s]'%(label_hv))
 
     ax31.text(0.05, 0.90, '(a)', color = 'w', transform=ax31.transAxes)
 
@@ -272,7 +275,7 @@ def plt_mono_vol(
     ax3.plot(
         #dxi['cents_cm'][1],
         #dxi['signal'][xind,:],
-        dxi['signal'][indx,:]*factor0,
+        dxi['signal'][indx,:]*scalex['hv']*factor0,
         dxi['cents_cm'][1],
         'bD-',
         label = 'XICSRT',
@@ -282,7 +285,7 @@ def plt_mono_vol(
     ax3.plot(
         #dtf['cents_cm'][1],
         #dtf['signal'][xind_tf,:]*scalet_0,
-        dtf['signal'][indxt,:]*scalet_0*factor0,
+        dtf['signal'][indxt,:]*scalet['hv']*factor0,
         dtf['cents_cm'][1],
         'ro-',
         label = 'ToFu',
@@ -294,7 +297,7 @@ def plt_mono_vol(
     #ax3.set_xlabel('vert. bin [cm]')
     #ax3.set_ylabel(r'signal [#ph/bin$^2$]')
     ax3.set_ylabel('vert. bin [cm]')
-    ax3.set_xlabel(r'signal [1e-11 #ph/bin$^2$]')
+    ax3.set_xlabel(r'signal [1e-11 %s]'%(label_hv))
 
     leg = ax3.legend(labelcolor='linecolor')
     leg.set_draggable('on')
@@ -316,7 +319,7 @@ def plt_mono_vol(
     ax3.plot(
         #dxi['cents_cm'][1],
         #np.sum(dxi['signal'],axis=0),
-        np.sum(dxi['signal'],axis=0)*factor1,
+        np.sum(dxi['signal'],axis=0)*scalex['v']*factor1,
         dxi['cents_cm'][1],
         'bD-',
         label = 'XICSRT',
@@ -326,7 +329,7 @@ def plt_mono_vol(
     ax3.plot(
         #dtf['cents_cm'][1],
         #np.sum(dtf['signal'],axis=0)*scalet_1,
-        np.sum(dtf['signal'],axis=0)*scalet_1*factor1,
+        np.sum(dtf['signal'],axis=0)*scalet['v']*factor1,
         dtf['cents_cm'][1],
         'ro-',
         label = 'ToFu',
@@ -338,7 +341,7 @@ def plt_mono_vol(
     #ax3.set_xlabel('vert. bin [cm]')
     #ax3.set_ylabel('signal [#ph/bin]')
     ax3.set_ylabel('vert. bin [cm]')
-    ax3.set_xlabel('signal [1e-10 #ph/bin]')
+    ax3.set_xlabel('signal [1e-10 %s]'%(label_v))
     
     #ax3.set_ylim(0,1.1*ymax)
     #ax3.set_xlim(0,1.1*ymax*factor1)
@@ -352,7 +355,7 @@ def plt_mono_vol(
 
     ax3.plot(
         dxi['cents_cm'][0],
-        dxi['signal'][:,indy]*factor0,
+        dxi['signal'][:,indy]*scalex['hv']*factor0,
         'bD-',
         label = 'XICSRT',
         linewidth = lw,
@@ -360,7 +363,7 @@ def plt_mono_vol(
         )
     ax3.plot(
         dtf['cents_cm'][0],
-        dtf['signal'][:,indyt]*scalet_0*factor0,
+        dtf['signal'][:,indyt]*scalet['hv']*factor0,
         'ro-',
         label = 'ToFu',
         linewidth = lw,
@@ -369,7 +372,7 @@ def plt_mono_vol(
 
     ax3.grid('on')
     ax3.set_xlabel('horiz. bin [cm]')
-    ax3.set_ylabel(r'signal [1e-11 #ph/bin$^2$]')
+    ax3.set_ylabel(r'signal [1e-11 %s]'%(label_hv))
 
     #leg = ax3.legend(labelcolor='linecolor')
     #leg.set_draggable('on')
@@ -391,7 +394,7 @@ def plt_mono_vol(
 
     ax3.plot(
         dxi['cents_cm'][0],
-        np.sum(dxi['signal'],axis=1)*factor1,
+        np.sum(dxi['signal'],axis=1)*scalex['h']*factor1,
         'bD-',
         label = 'XICSRT',
         linewidth = lw,
@@ -399,7 +402,7 @@ def plt_mono_vol(
         )
     ax3.plot(
         dtf['cents_cm'][0],
-        np.sum(dtf['signal'],axis=1)*scalet_00*factor1,
+        np.sum(dtf['signal'],axis=1)*scalet['h']*factor1,
         'ro-',
         label = 'ToFu',
         linewidth = lw,
@@ -409,7 +412,7 @@ def plt_mono_vol(
     ax3.set_title('int. over all vert. bin', pad = 0)
     ax3.grid('on')
     ax3.set_xlabel('horiz. bin [cm]')
-    ax3.set_ylabel('signal [1e-10 #ph/bin]')
+    ax3.set_ylabel('signal [1e-10 %s]'%(label_h))
     
     #ax3.set_ylim(0,1.1*ymax*factor1)
     ax3.set_xlim(-0.25, -0.1)
