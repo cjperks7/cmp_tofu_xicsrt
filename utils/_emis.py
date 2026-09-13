680f4066a7eec1ff7ad899d5017dbd3d7ea55e9b
cjperks7 <cjperk@mit.edu>
2026-09-13 13:22:52 -0400

    debug running emis v6

--- utils/_emis.py
+++ utils/_emis.py
@@ -141,7 +141,7 @@ def _prep_emis_xicsrt(
     # Output
     return {
         'emis': (
-            coll.ddata['emis_'+key_diag]['data'][:,:,ilamb]
+            coll.ddata['emis']['data'][:,:,ilamb]
             * (4*np.pi) *1e-6 * 1e-10 # Converts from ph/s/m3/sr/m -> ph/s/cm3/AA
             ), # dim(mesh_R, mesh_Z); [ph/s/cm3/AA]
         'mesh_R': coll.ddata['%s_k0'%(key_mesh)]['data'], # dim(mesh_R); [m]
